629f396bfd9b378e580ccff1b8d69c7e0fa7a2cb
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-10-13 19:28:44 -0400

    ignore unused imports for flake8

--- Interactive_Map/map/urls.py
+++ Interactive_Map/map/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HomePageView, Mapview
 
 urlpatterns = [
-path('', HomePageView.as_view(), name='home'),
-path('map/', Mapview.as_view(), name='map'),
+    path("", HomePageView.as_view(), name="home"),
+    path("map/", Mapview.as_view(), name="map"),
 ]
--- Interactive_Map/map/views.py
+++ Interactive_Map/map/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render #noqa: F401
+from django.http import HttpResponse #noqa: F401
 from django.views.generic import TemplateView
 
+
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
+
 
 class Mapview(TemplateView):
-    template_name= 'map.html'
+    template_name = "map.html"
