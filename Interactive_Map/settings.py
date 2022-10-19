8e98477dc929a0dddc00819186280f64f869d8af
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-10-19 16:54:18 -0400

    removed secret key from public

--- Interactive_Map/settings.py
+++ Interactive_Map/settings.py
@@ -20,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3*$=^f#m(ct&vp)tju09&=(i32_o*b_8$5smwqlj%m4ev@7y*q"
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
