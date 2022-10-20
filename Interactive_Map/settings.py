0b568a0c21266171dbf4aded0d484459ea5d682c
AB1738 <98786667+AB1738@users.noreply.github.com>
2022-10-19 22:00:22 -0400

    Changed database from sqlite3 to postgresql for heroku deployment

--- Interactive_Map/settings.py
+++ Interactive_Map/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "Interactive_Map.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'MibfauWjmSgS1bbXL431',
+        'HOST': 'containers-us-west-103.railway.app',
+        'PORT': '5895',
     }
 }
 
