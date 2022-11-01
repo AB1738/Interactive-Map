32106663a92e1aae9b99a23a8d035c5cde6ea87e
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-11-01 03:33:55 -0400

    removed grocery store and added farmer_market test

--- map/tests.py
+++ map/tests.py
@@ -1,18 +1,18 @@
 from django.test import TestCase 
-from map.models import GroceryStoreAddresses
+from map.models import GroceryStoreAddresses, FarmersMarketAddresses
 import math
 import geocoder
 # Create your tests here.
 
-class GroceryStoreAddressProcessingTestCase(TestCase):
+class FarmerMarketAddressProcessingTestCase(TestCase):
     def test_address_processing(self):
         access_token='pk.eyJ1IjoiaGFtc2llIiwiYSI6ImNsODN4aWdmcjBhNHEzcGw4ZXYxMHcxaXkifQ.67o9saEURWg3rF02gZxGKg'
-        testModle = GroceryStoreAddresses(address="115 E 68th St, New York, NY 10065")
-        g=geocoder.mapbox(testModle.address,key=access_token)
+        testModel = FarmersMarketAddresses(farmer_address="535 MARCY AVE")
+        g=geocoder.mapbox(testModel.farmer_address,key=access_token)
         latlong = g.latlng
         print(math.trunc(latlong[0]))
         print(math.trunc(latlong[1]))
-        return(math.trunc(latlong[0])==40 and math.trunc(latlong[1])==-73)
-
+        self.assertEqual(math.trunc(latlong[0]),40)
+        self.assertEqual(math.trunc(latlong[1]), -73)
 
 
