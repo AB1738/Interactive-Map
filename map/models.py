09a755ce18a1e14cd8eabccb8dbe026e4d1410b1
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-11-01 02:38:09 -0400

    removed save override

--- map/models.py
+++ map/models.py
@@ -1,5 +1,6 @@
 from django.db import models 
 import geocoder
+import csv
 
 
 access_token='pk.eyJ1IjoiaGFtc2llIiwiYSI6ImNsODN4aWdmcjBhNHEzcGw4ZXYxMHcxaXkifQ.67o9saEURWg3rF02gZxGKg'
@@ -7,13 +8,6 @@ class GroceryStoreAddresses(models.Model):
     address=models.TextField()
     lat=models.FloatField(blank=True,null=True)
     long=models.FloatField(blank=True,null=True)
-
-    def save(self, *args, **kwargs):
-        g=geocoder.mapbox(self.address,key=access_token)
-        g=g.latlng
-        self.lat=g[0]
-        self.long=g[1]
-        return super(GroceryStoreAddresses,self).save(*args,**kwargs)
 
 class FarmersMarketAddresses(models.Model):
     farmer_address=models.TextField()
