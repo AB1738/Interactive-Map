64d9fb535ddae23a99d7528434a27a4abca4444b
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-11-17 20:12:35 -0500

    fixed travis errors

--- map/tests.py
+++ map/tests.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from map.models import GroceryStoreAddresses, FarmersMarketAddresses, FireHouseAddresses
-import math
 import geocoder
 import time
 
