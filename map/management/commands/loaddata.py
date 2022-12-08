8e798e39ae41049d39a908b38b47419f55c4be77
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-12-08 17:18:03 -0500

    added supercenters and supermarkets to db

--- map/management/commands/loaddata.py
+++ map/management/commands/loaddata.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from map.models import GroceryStoreAddresses, FarmersMarketAddresses, FireHouseAddresses
+from map.models import GroceryStoreAddresses, FarmersMarketAddresses, FireHouseAddresses, SupercenterAddresses, SuperMarketAddresses
 from enum import Enum
 import csv
 import traceback
@@ -9,6 +9,8 @@ class ModelType(Enum):
     FarmerMarket = 1
     GroceryStore = 2
     Firehouses = 3
+    Supercenters = 4
+    Supermarkets = 5
 
 
 class Command(BaseCommand):
@@ -32,6 +34,12 @@ class Command(BaseCommand):
                     elif kwargs['ObjectType'] == ModelType.Firehouses:
                         if row['Latitude'] != '' and row['Longitude'] != '':
                             FireHouseAddresses.objects.create(lat=row['Latitude'], long=row['Longitude'])
+                    elif kwargs['ObjectType'] == ModelType.Supermarkets:
+                        if row['Latitude'] != '' and row['Longitude'] != '':
+                            SuperMarketAddresses.objects.create(lat=row['Latitude'], long=row['Longitude'])
+                    elif kwargs['ObjectType'] == ModelType.Supercenters:
+                        if row['Latitude'] != '' and row['Longitude'] != '':
+                            SupercenterAddresses.objects.create(lat=row['Latitude'], long=row['Longitude'])
                     else:
                         return
         except Exception:
@@ -40,11 +48,17 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         DATA_ROOT = './map/data/'
         FARMER_MARKET_PATH = DATA_ROOT + 'farmer_market.csv'
-        GROCERY_STORE_PATH = DATA_ROOT + 'Retail_Food_Stores.csv'
+        GROCERY_STORE_PATH = DATA_ROOT + 'grocery_stores.csv'
         FIREHOUSE_PATH = DATA_ROOT + 'firehouses.csv'
+        SUPERCENTERS_PATH = DATA_ROOT + 'supercenters.csv'
+        SUPERMARKETS_PATH = DATA_ROOT + 'supermarkets.csv'
         if not FarmersMarketAddresses.objects.exists():
             self.__process_csv(ObjectType=ModelType.FarmerMarket, Path=FARMER_MARKET_PATH)
         if not GroceryStoreAddresses.objects.exists():
             self.__process_csv(ObjectType=ModelType.GroceryStore, Path=GROCERY_STORE_PATH)
         if not FireHouseAddresses.objects.exists():
             self.__process_csv(ObjectType=ModelType.Firehouses, Path=FIREHOUSE_PATH)
+        if not SuperMarketAddresses.objects.exists():
+            self.__process_csv(ObjectType=ModelType.Supermarkets, Path=SUPERMARKETS_PATH)
+        if not SupercenterAddresses.objects.exists():
+            self.__process_csv(ObjectType=ModelType.Supercenters, Path=SUPERCENTERS_PATH)
